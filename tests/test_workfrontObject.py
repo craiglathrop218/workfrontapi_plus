fa6b0041385721a1688d72b3f262857eca69269d
craiglathrop218 <craiglathrop218@gmail.com>
2017-09-04 09:42:06 -0700

    Update to 1.0.16 and various unit test updates.

--- tests/test_workfrontObject.py
+++ tests/test_workfrontObject.py
@@ -1,13 +1,27 @@
 from unittest import TestCase
 from workfrontapi_plus.core_wf_object import WorkfrontObject
 from tests.wf_project_data_mock import project_data
+from datetime import datetime
+
 
 class TestWorkfrontObject(TestCase):
 
     core_obj = WorkfrontObject(project_data)
 
     def test__convert_dates(self):
-        pass
+
+        res = self.core_obj._convert_dates(project_data)
+        golden = '2015-09-18'
+        test = res['entryDate'].strftime('%Y-%m-%d')
+        self.assertEqual(test, golden)
+
+        golden = 'Test 2'
+        test = res['name']
+        self.assertEqual(test, golden)
+
+        test = res['projectUsers'][0]['user']['reservedTimes'][0]['endDate']
+        self.assertTrue(isinstance(test, datetime))
+
 
     def test_save(self):
         pass
